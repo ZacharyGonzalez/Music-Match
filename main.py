d0fc189989e1321239288baa61f61f3641980e61
Venrite <146303521+Venrite@users.noreply.github.com>
2025-08-31 12:31:40 -0400

    testing data colletion

--- main.py
+++ main.py
@@ -3,27 +3,15 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
-import requests
-
+import json
 load_dotenv()
 
-CLIENT_SECRET=os.getenv("SPOTIFY_TOKEN")
+CLIENT_SECRET=os.getenv("CLIENT_SECRET")
 CLIENT_ID=os.getenv("CLIENT_ID")
 REDIRECT_URI="http://127.0.0.1:8080"
+print(CLIENT_SECRET)
+print(CLIENT_ID)
 
-url = "https://spotifycharts.com/regional/global/daily/latest/download"
-r=requests.get(url,allow_redirects=True)
-r.raise_for_status()
-df = pd.read_csv(r.text)
-print(df.head())
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
-
-"""
 scope = "user-library-read"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=CLIENT_ID,
@@ -31,12 +19,23 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri=REDIRECT_URI,
     scope=scope))
 
-results = sp.search(q="track:doxy", type="track", limit=10)
-for track in results:
-    print(track['name'], "-", track['artists'][0]['name'])
+df = pd.read_csv('regional-us-weekly-2025-08-28.csv')
+spotify_uri=df['uri'][0]
+print(df)
+results = sp.track(spotify_uri)
+print(json.dumps(results,indent=2))
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
 
 
 
+
+
+"""
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
     track = item['track']
